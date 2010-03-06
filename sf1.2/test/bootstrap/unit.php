163622de75b0b382822889d2463361b53349ce15
Thomas Rabaix <thomas.rabaix@soleoweb.com>
2010-03-06 15:16:19 +0000

    oups ...
    git-svn-id: http://svn.symfony-project.com/plugins/mgI18nPlugin/tags/VERSION_1_2_7@28412 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- sf1.2/test/bootstrap/unit.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$base_project = dirname(__FILE__).'/../../../../';
-
-
-require_once $base_project.'/config/ProjectConfiguration.class.php';
-$configuration = new ProjectConfiguration($base_project);
-include($configuration->getSymfonyLibDir().'/vendor/lime/lime.php');
